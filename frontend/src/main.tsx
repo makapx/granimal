4716e436d700f2868a6e5881f5cd4dbd618f35da
Marco Spampinato <cantabokca598@gmail.com>
2024-03-27 12:48:45 +0100

    feat: installed react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,23 @@
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Provider } from 'react-redux'
 import store from './redux/store'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App/>
+}])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
-  </Provider>,
+        <RouterProvider router={router}/>
+    </Provider>,
   </React.StrictMode>,
 )
