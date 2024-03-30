6d357fe0638236432279fccad69072eb85054a26
marcospampi <cantabokca598@gmail.com>
2024-03-30 19:47:21 +0100

    feat: user apis for frontend

---
+++ frontend/src/api/user.api.ts
@@ -0,0 +1,89 @@
+export type CreateUserParams = {
+  username: string;
+  password: string;
+  picture?: string;
+};
+export type LoginParams = {
+  username: string;
+  password: string;
+};
+export type ChangePasswordParams = {
+  password: string;
+};
+export type ChangePictureParams = {
+  picture: string;
+};
+
+export type TokenResult = { token: string };
+
+/**
+ * Creates a user
+ * @param params 
+ * @returns a new token
+ */
+export function createUser(params: CreateUserParams): Promise<TokenResult> {
+  return fetch('/api/auth/signup', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(params)
+  }).then(result => result.ok
+    ? result.json() as Promise<TokenResult>
+    : Promise.reject(result)
+  );
+}
+
+/**
+ * Authenticates a user
+ * @param params 
+ * @returns a new token
+ */
+export function loginUser(params: LoginParams): Promise<TokenResult> {
+  return fetch('/api/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(params)
+  }).then(result => result.ok
+    ? result.json() as Promise<TokenResult>
+    : Promise.reject(result)
+  );
+}
+
+/**
+ * Changes user's password
+ * @param params 
+ * @param token the user's authorized token
+ * @returns a new token
+ */
+export function changeUserPassword(params: ChangePasswordParams, token: string): Promise<TokenResult> {
+  return fetch('/api/user/password', {
+    method: 'PATCH',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(params)
+  }).then(result => result.ok
+    ? result.json() as Promise<TokenResult>
+    : Promise.reject(result)
+  );
+}
+
+/**
+ * Changes user's profile picture
+ * @param params 
+ * @param token the user's authorized token
+ * @returns a new token
+ */
+export function changeUserPicture(params: ChangePictureParams, token: string): Promise<TokenResult> {
+  return fetch('/api/user/password', {
+    method: 'PATCH',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(params)
+  }).then(result => result.ok
+    ? result.json() as Promise<TokenResult>
+    : Promise.reject(result)
+  );
+}
