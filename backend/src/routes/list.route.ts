9ad92a9205eadc1e4a7a90869c4c4c95fbcbf6fc
Marco Spampinato <cantabokca598@gmail.com>
2024-04-12 14:30:05 +0200

    refactor: simplify function names, extract lists providers

--- backend/src/routes/list.route.ts
+++ backend/src/routes/list.route.ts
@@ -1,31 +1,34 @@
 import { FastifyInstance, FastifyRequest } from "fastify";
 import { WebError } from "../misc/error";
-import { deleteAnimeOfUser, getTrackingListOfUser, trackAnime } from "../models/lists.model";
+import { removeFromList, getList, putIntoList } from "../providers/lists.providers";
 import { authenticate } from "../providers/user.providers";
 import { TrackingList } from "../types/trackinglist";
 import { UserType } from "../types/user";
 
 export default function ( app: FastifyInstance, opts: unknown, done: Function ) {
 
-    app.get('/:userId', (req: FastifyRequest<{ Params: { userId: number } }>) => {
-        return getTrackingListOfUser(req.params.userId);
+    app.get('/:userId', (req: FastifyRequest<{ Params: { userId: number|'me' } }>) => {
+        const userId: number = req.params.userId === 'me' 
+            ? (req.user as UserType)?.id as number ?? req.params.userId
+            :  req.params.userId; 
+        return getList(userId);
     });
 
     app.put('/:userId/:animeId', { preHandler: authenticate }, (req: FastifyRequest<{Body: Partial<TrackingList>, Params: { userId: number, animeId: number } }>) => {
         const user = req.user as UserType;
-        return trackAnime(user.id as number, req.params.animeId, req.body);
+        return putIntoList(user.id as number, req.params.animeId, req.body);
     });
 
     app.delete('/:userId/:animeId', { preHandler: authenticate }, async (req: FastifyRequest<{ Params: { userId: number, animeId: number } }>, reply) => {
         const user = req.user as UserType;
-        const deleted = await deleteAnimeOfUser(user.id as number, req.params.animeId);
+        const deleted = await removeFromList(user.id as number, req.params.animeId);
         if ( deleted === 0)
             reply.status(304);
     });
 
     app.patch('/:userId/:animeId', { preHandler: authenticate }, (req: FastifyRequest<{Body: Partial<TrackingList>, Params: { userId: number, animeId: number } }>, reply) => {
         const user = req.user as UserType;
-        return trackAnime(user.id as number, req.params.animeId, req.body)
+        return putIntoList(user.id as number, req.params.animeId, req.body)
             .catch(() => {throw new WebError(400, 'user', 'User not found')});
     });
 
